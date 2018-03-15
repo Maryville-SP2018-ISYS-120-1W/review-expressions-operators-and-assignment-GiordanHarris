956fd6fcfbfe6471cc611a6024ec92e7d667152f
GiordanHarris <33729645+GiordanHarris@users.noreply.github.com>
2018-03-14 22:32:00 -0400

    Add files via upload

--- ExpressionsAndAssignmentReview.playground/Contents.swift
+++ ExpressionsAndAssignmentReview.playground/Contents.swift
@@ -15,3 +15,36 @@
  
 */
 
+/*
+ For this assignemt, you are a fisherman, and you are building an app that will help you and your co workers easily keep track of the amount and nubmer of fish you catch. the first thing you need to do is declare a constant for the app, we will name it fishPrice, and we will give it a value of 10.5, whch is the price wyou charge per fish caught. list your constant below:
+*/
+ 
+ /*
+we also need a variable, for this app, fishCaught which for now will be set to zero. set the values below
+ */
+
+/*
+ On your first fishing trip out on the boat, you and your friends catch a decent amount of fish, 12 of them, add this to your fishCagught and print the result
+ */
+ 
+/*
+ On the boat ride back, your friend knocks over the cooler and drops 1/2 of the bucket over the boat, update your fishCaught variable by dividing the current number by 2
+ */
+ 
+ /*
+ Luckily you have enough time to throw your lure in and try and catch a coule more fish, you get lucky and catch two more! adjust your fishCaught var using a compound operating to show the new result, and print it
+ */
+ 
+ /*
+ you finally arrive at the fish market and are going to be paid for all of your hard efforts, but we need to know how much to pay you. create a new variable labeled pay, which equals fishCaught multiplied by fishPrice, print the result
+ */
+ 
+ /*
+ if the code does not compile, set it as a double, and print the result again
+ */
+
+/*
+ did it work that time? yes? congratulations!
+ */
+
+
